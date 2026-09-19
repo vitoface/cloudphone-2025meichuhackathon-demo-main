07c934f857a7fa2fc0bd5565f1b819cbf4c5e369
SKRman6687 <pinray95@gmail.com>
2026-09-19 16:42:49 +0800

    解決npm run build不能跑的問題

--- app/page.tsx
+++ app/page.tsx
@@ -267,7 +267,9 @@ export default function HomePage() {
 
   // 5. 初始化 Leaflet
   useEffect(() => {
-    if (!mapContainerRef.current) return;
+    const mapContainer = mapContainerRef.current;
+
+    if (!mapContainer) return;
 
     let isMounted = true;
 
@@ -293,7 +295,7 @@ export default function HomePage() {
 
         const initialZoom = savedZoom ? parseInt(savedZoom, 10) : 17;
 
-        const map = L.map(mapContainerRef.current, {
+        const map = L.map(mapContainer, {
           center: initialCenter,
           zoom: initialZoom,
           zoomControl: false,
